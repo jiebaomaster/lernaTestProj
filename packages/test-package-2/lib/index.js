7b72ab95fc2416ae1b7d9d1344b72e62c245a439
chenjiehong <chenjiehong@epiclouds.net>
2018-11-28 15:00:38 +0800

    add function

---
+++ packages/test-package-2/lib/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = sayHello;
+
+function sayHello() {
+	console.log('hello word')
+}
--- packages/test-package-2/lib/test-package-2.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = testPackage2;
-
-function testPackage2() {
-    // TODO
-}
