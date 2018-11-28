ff411d2320dc4cd10fdb2d06c8bedd9bc27460de
chenjiehong <chenjiehong@epiclouds.net>
2018-11-28 20:07:23 +0800

    dependency scope package

--- packages/test-new-branch/lib/test-new-branch.js
+++ packages/test-new-branch/lib/test-new-branch.js
@@ -1,7 +1,12 @@
 'use strict';
 
+var sayHello = require('@jbmasterlerna/test-package-2')
+var babel_eslint = require('babel-eslint')
+
 module.exports = testNewBranch;
 
 function testNewBranch() {
     // TODO
+    sayHello()
 }
+
