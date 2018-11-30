e0f20cde4f883a9aff23f0cc811e53031025c0e0
chenjiehong <chenjiehong@epiclouds.net>
2018-11-30 15:17:36 +0800

    mod script

--- script/release.js
+++ script/release.js
@@ -1,13 +1,15 @@
 /* package发布 */
 
 const execa = require('execa')
+const chalk = require('chalk')
+const log = console.log
 
 const release = async () => {
   // 获取已更新的 package
   const lerna_changed_result = await execa('lerna', ['changed'])
   const changedPackages = lerna_changed_result.stdout
-  console.log('> ', lerna_changed_result.cmd)
-  console.log('*****changed packages:******* \n', changedPackages)
+  log(chalk.bold.blue(`> ${lerna_changed_result.cmd}`))
+  log(chalk.bold('*************changed packages:************* \n'), changedPackages)
 
   // 给每个已更新的包运行 npm run build 命令，打包  
   let scope = changedPackages.split('\n').join(',--scope,').split(',')
@@ -20,10 +22,11 @@ const release = async () => {
     'run',
     'build'
   ])
-  console.log('> ', lerna_exec_result.cmd)
-  console.log(lerna_exec_result.stdout)
+  log(chalk.bold.blue(`> ${lerna_exec_result.cmd}`))
+  log(lerna_exec_result.stdout)
+  log(chalk.bold.blue(`> lerna publish`))
 }
 
 release().catch(err => {
-  console.log(err)
+  log(err)
 })
